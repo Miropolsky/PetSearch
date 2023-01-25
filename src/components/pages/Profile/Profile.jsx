4a9b9160cb51d5e167980672911842a087013b39
Miropolsky <Miropolskysk@yandex.ru>
2023-01-25 12:43:06 +0500

    profilehtml

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -1,5 +1,35 @@
 import styles from './Profile.module.scss';
 
 export default function Profile() {
-    return <div className={styles.container}>Страница профиля</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.whiteBackground}>
+                <div className={styles.containerProfile}>
+                    <div className={styles.logoProfile}>Мой профиль</div>
+                    <div className={styles.content}>
+                        <div className={styles.menu}>
+                            <div className={styles.menuItem}>
+                                Мои объявления
+                            </div>
+                            <div className={styles.menuItem}>
+                                Добавить объявление
+                            </div>
+                            <div className={styles.menuItem}>Избранное</div>
+                            <div className={styles.menuItem}>Настройка</div>
+                            <div className={styles.exit}>Выйти</div>
+                        </div>
+                        <div className={styles.myAds}>
+                            <div className={styles.titleAds}>
+                                Мои объявления
+                            </div>
+                            <div className={styles.ads}>
+                                У вас пока нет своих объявлений. Можете их
+                                добавить тут
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
